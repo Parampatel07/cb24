c4026b50366ce875c503bc397a389a1d6cfda42c
parampatel07 <iamparam102@gmail.com>
2024-02-07 17:58:19 +0530

    array

--- array5.c
+++ array5.c
@@ -6,7 +6,9 @@ void main()
      int students[5][5];
      int count = 0;
      int flash = 0;
-
+     int total[5];
+     int student_total = 0;
+     float average = 0;
      for (flash = 0; flash < 5; flash++)
      {
           printf("\nEnter marks for student %d \n", flash + 1);
@@ -16,7 +18,6 @@ void main()
                scanf("%d", &students[flash][count]);
           }
      }
-
      for (flash = 0; flash < 5; flash++)
      {
           printf("\nMarks for student %d ", flash + 1);
@@ -25,6 +26,53 @@ void main()
                printf("\nMarks of subject %d = %d ", count + 1, students[flash][count]);
           }
      }
+
+     for (flash = 0; flash < 5; flash++)
+     {
+          total[flash] = 0;
+          for (count = 0; count < 5; count++)
+          {
+               total[flash] = students[flash][count] + total[flash];
+          }
+          printf("\nThe value of total for student %d is %d ", flash + 1, total[flash]);
+     }
+
+     for (count = 0; count < 5; count++)
+     {
+          student_total = total[count] + student_total;
+     }
+     printf("\nThe total of class is %d ", student_total);
+
+     average = student_total / 5;
+     printf("\nThe value of average is %f ", average);
+     // student_total = total[1] + student_total;
+     // student_total = total[2] + student_total;
+     // student_total = total[3] + student_total;
+     // student_total = total[4] + student_total;
+
+     // total[1] = 0;
+     // for (count = 0; count < 5; count++)
+     // {
+     //      total[1] = students[1][count] + total[1];
+     // }
+     // printf("\nThe value of total for student 2 is %d ", total[1]);
+     // total[2] = 0;
+     // for(count = 0 ; count < 5 ; count++)
+     // {
+     //      total[2] = students[2][count] + total[2];
+     // }
+     // printf("\nThe value of total for student 3 is %d ", total[2]);
+     // total[2] = students[2][1] + total[2];
+     // total[2] = students[2][2] + total[2]
+     // total[1] = students[1][1] + total[1];
+     // total[1] = students[1][2] + total[1];
+     // total[1] = students[1][3] + total[1];
+     // total[1] = students[1][4] + total[1];
+     // total[0] = students[0][1] + total[0];
+     // total[0] = students[0][2] + total[0];
+     // total[0] = students[0][3] + total[0];
+     // total[0] = students[0][4] + total[0];
+
      // printf("\nMarks for student 2 ");
      // for (count = 0; count < 5; count++)
      // {
